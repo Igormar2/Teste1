168eea3c4ed40f406460e37cd335ac17033f1292
Mascavo <gui12asf@gmail.com>
2025-09-24 16:40:28 -0300

    O Jogo.c

--- O Jogo.c
+++ O Jogo.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -24,47 +17,47 @@ int main(){
     
     scanf("%s", n1);
     
-    printf("%s, \n vamos começar o jogo, faca sua escolha rapido!!", n1);
+    printf("%s, \n vamos começar o jogo, faca sua escolha rapido!!\n", n1);
     
-    printf("\n opcões: 1 = Pedra, 2 = Papel 3 = Tesoura ");
+    printf("opcões: 1 = Pedra, 2 = Papel 3 = Tesoura\n ");
     scanf("%d", &J1);
-    
+    srand(time(NULL));
     M1 = rand() % 3 + 1;
     
     
     
     if (J1 == 1){
         if (M1 == 2){
-            printf(" %s :Pedra", n1);
-            printf(" M1 :papel...");
+            printf("%s: Pedra\n", n1);
+            printf("M1: papel...\n");
             printf("embrulhado");
         }
         if (M1 == 3){
-            printf(" %s :Pedra", n1);
-            printf(" M1 :Tesoura...");
+            printf("%s: Pedra\n", n1);
+            printf("M1: Tesoura...\n");
              printf("Amassou");
         }
         if (M1 == 1){
-           printf(" %s :Pedra", n1);
-           printf(" M1 :Pedra...");
+           printf("%s: Pedra\n ", n1);
+           printf("M1: Pedra... \n");
            printf("Empate técnico");
     }
     }
     
       if (J1 == 2){
         if (M1 == 3 ){
-           printf(" %s :Papel", n1);
-           printf(" M1 :Tesoura...");
+           printf("%s: Papel\n", n1);
+           printf("M1: Tesoura...\n");
            printf("PATO");
         }
         if (M1 == 1){
-             printf(" %s :Papel", n1);
-             printf(" M1 :Pedra...");
+             printf("%s: Papel\n", n1);
+             printf("M1: Pedra...\n");
              printf("Amassou");
         }
         if (M1 == 2){
-               printf(" %s :Papel", n1);
-               printf(" M1 :Papel...");
+               printf("%s: Papel\n", n1);
+               printf("M1: Papel...\n");
                printf("vish.. deu velha");
     
         }
@@ -72,18 +65,18 @@ int main(){
     
       if (J1 == 3){
         if (M1 == 1 ){
-            printf(" %s :Tesoura", n1);
-            printf(" M1 :Pedra...");
+            printf("%s: Tesoura\n", n1);
+            printf("M1: Pedra...\n");
             printf("Volta pra adedanha");
         }
         if (M1 == 2 ){
-             printf(" %s :Tesoura", n1);
-             printf(" M1 :Papel...");
+             printf("%s: Tesoura\n", n1);
+             printf("M1: Papel...\n");
              printf("Amassou");
         }
          if (M1 == 3){
-               printf(" %s :Tesoura", n1);
-               printf(" M1 :Tesoura...");
+               printf("%s: Tesoura\n", n1);
+               printf("M1: Tesoura...\n");
                printf("vish.. deu velha !?");
     
         }
