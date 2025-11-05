a02143de3b330f67e24fdbad630e6579b440e3bf
IGOR MARÇAL <igorm4758@gmail.com>
2025-11-05 16:41:06 -0300

    Update _jogo_da_forca_com_TXTmain.c

--- _jogo_da_forca_main.c
+++ _jogo_da_forca_main.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
+void inicializar_aleatoriedade() { 
+srand(time(NULL));
+
+}
+
+int main(){
+    inicializar_aleatoriedade();
     int erros = 0;
     int max_tentativas = 6;
-    int acertou = 0; // Flag booleana (0 ou 1)
-    int enforcou = 0; // Flag booleana (0 ou 1)
-    char palavra_secreta[] = "LOGICA";
-    char display[] = "______";
+    int acertou = 0; 
+    int enforcou = 0; 
+    FILE* f = fopen("Palavras.txt", "r");
+
+    if (f == NULL) {
+    printf("Erro: Banco de palavras nao encontrado.\n");
+    exit(1); 
+
+    }
+
+
+    int total_palavras = 0;
+    fscanf(f, "%d", &total_palavras);
+
+    int indice_sorteado = rand() % total_palavras; 
+    char palavra_secreta[50];
+   
+
+    for (int i = 0; i <= indice_sorteado; i++) {
+    fscanf(f, "%s", palavra_secreta);
+    }
+
     int tamanho = strlen(palavra_secreta);
+    
+    char display[50];
+    for (int i = 0; i < tamanho; i++){
+        display[i] = '_';
+    }
+    display[tamanho] = '\0';
     char tentativa; 
     char cpdisplay[50];
+    
+
+    
     
     while (!acertou &&!enforcou)
     {
@@ -19,6 +55,7 @@ int main()
     
     printf("...\n");
     scanf(" %c", &tentativa);
+    tentativa = toupper(tentativa); 
     for(int i = 0; i<tamanho ; i++)
     {
         if(tentativa == palavra_secreta[i])
